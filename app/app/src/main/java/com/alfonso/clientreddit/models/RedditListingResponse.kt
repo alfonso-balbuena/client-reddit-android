ad74c26f8579ce3487262928f6bb841aba4ea779
Alfonso Balbuena Sánchez <alfonso1011@gmail.com>
2021-04-21 22:15:58 -0500

    Model for the Reddit response top

--- app/app/src/main/java/com/alfonso/clientreddit/models/RedditListingResponse.kt
+++ app/app/src/main/java/com/alfonso/clientreddit/models/RedditListingResponse.kt
@@ -1,3 +1,9 @@
 package com.alfonso.clientreddit.models
 
-data class RedditListingResponse()
+data class RedditListingChildData(val  title : String, val num_comments : Int,val author : String, val thumbnail : String, val created_utc : Long)
+
+data class RedditListingChild(val kind : String,val data : RedditListingChildData)
+
+data class RedditListingData(val modhash : String,val dist : Int,val children : List<RedditListingChild>)
+
+data class RedditListingResponse(val data : RedditListingData)
