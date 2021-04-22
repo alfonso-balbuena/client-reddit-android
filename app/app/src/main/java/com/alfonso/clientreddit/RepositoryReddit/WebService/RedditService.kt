8ed516644a8fd6c7d8e32ece9aa3e34d0cae93ae
Alfonso Balbuena Sánchez <alfonso1011@gmail.com>
2021-04-21 19:38:35 -0500

    Added the return type for the access token

--- app/app/src/main/java/com/alfonso/clientreddit/RepositoryReddit/WebService/RedditService.kt
+++ app/app/src/main/java/com/alfonso/clientreddit/RepositoryReddit/WebService/RedditService.kt
@@ -1,5 +1,6 @@
 package com.alfonso.clientreddit.RepositoryReddit.WebService
 
+import com.alfonso.clientreddit.models.AccessToken
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.Header
@@ -8,5 +9,5 @@ import retrofit2.http.POST
 public interface RedditService {
     @FormUrlEncoded
     @POST("access_token")
-    suspend fun getAccessToken(@Header("Authorization") auth : String,@Field("grant_type") grantType : String, @Field("device_id") deviceId : String )
+    suspend fun getAccessToken(@Header("Authorization") auth : String,@Field("grant_type") grantType : String, @Field("device_id") deviceId : String ) : AccessToken
 }
