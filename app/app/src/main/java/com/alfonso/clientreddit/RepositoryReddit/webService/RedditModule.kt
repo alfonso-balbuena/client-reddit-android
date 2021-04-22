2d1c0a8031763ce4797df00982f00e71fa2f08de
Alfonso Balbuena Sánchez <alfonso1011@gmail.com>
2021-04-21 22:16:31 -0500

    Service
    Add the service for getting the top post
    Add the module for DI

--- app/app/src/main/java/com/alfonso/clientreddit/RepositoryReddit/webService/RedditModule.kt
+++ app/app/src/main/java/com/alfonso/clientreddit/RepositoryReddit/webService/RedditModule.kt
@@ -1,4 +1,4 @@
-package com.alfonso.clientreddit.RepositoryReddit.WebService
+package com.alfonso.clientreddit.RepositoryReddit.webService
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
@@ -13,9 +13,18 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 @InstallIn(SingletonComponent::class)
 internal object RedditModule {
     @Provides
-    fun provideRedditService() : RedditService {
+    fun provideRedditServiceToken() : RedditServiceToken {
         val moshi  = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
         val retrofit = Retrofit.Builder().baseUrl("https://www.reddit.com/api/v1/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+        return retrofit.create(RedditServiceToken::class.java)
+    }
+
+    @Provides
+    fun provideRedditService() : RedditService {
+        val moshi  = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+        val retrofit = Retrofit.Builder().baseUrl("https://oauth.reddit.com/")
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
         return retrofit.create(RedditService::class.java)
--- app/app/src/main/java/com/alfonso/clientreddit/RepositoryReddit/webService/RedditService.kt
+++ app/app/src/main/java/com/alfonso/clientreddit/RepositoryReddit/webService/RedditService.kt
@@ -1,4 +1,12 @@
 package com.alfonso.clientreddit.RepositoryReddit.webService
 
+import com.alfonso.clientreddit.models.RedditListingResponse
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
 interface RedditService {
+
+    @GET("r/subreddit/top")
+    suspend fun getTop(@Header("Authorization") auth : String,@Query("t") t : String,@Query("count") count : Int, @Query("limit") limit : Int) : RedditListingResponse
 }
