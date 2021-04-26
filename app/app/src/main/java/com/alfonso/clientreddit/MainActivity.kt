598319971e819018cfc821458d440b79e9678b97
Alfonso Balbuena Sánchez <alfonso1011@gmail.com>
2021-04-26 13:31:13 -0500

    Binding layout
    -Inflate layout with binding
    - DrawerLayout for portrait

--- app/app/src/main/java/com/alfonso/clientreddit/MainActivity.kt
+++ app/app/src/main/java/com/alfonso/clientreddit/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.alfonso.clientreddit
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
 import androidx.activity.viewModels
+import androidx.core.view.GestureDetectorCompat
 import com.alfonso.clientreddit.viewModel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
+import com.alfonso.clientreddit.databinding.ActivityMainBinding
+import timber.log.Timber
+
+
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    val viewModel : MainViewModel by viewModels()
+    private val viewModel : MainViewModel by viewModels()
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d("MainActivity",viewModel.toString())
-        setContentView(R.layout.activity_main)
+       super.onCreate(savedInstanceState)
+       binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
