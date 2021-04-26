0dfa80101029dca421fd3cdba4035d589c9068fc
Alfonso Balbuena Sánchez <alfonso1011@gmail.com>
2021-04-26 18:54:40 -0500

    Animation for dismiss item

--- app/app/src/main/java/com/alfonso/clientreddit/fragments/PostList.kt
+++ app/app/src/main/java/com/alfonso/clientreddit/fragments/PostList.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -18,6 +20,7 @@ import com.alfonso.clientreddit.adapter.PostListener
 import com.alfonso.clientreddit.databinding.FragmentPostListBinding
 import com.alfonso.clientreddit.viewModel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import jp.wasabeef.recyclerview.animators.SlideInLeftAnimator
 import timber.log.Timber
 
 @AndroidEntryPoint
@@ -25,6 +28,7 @@ class PostList : Fragment() {
 
     private lateinit var binding : FragmentPostListBinding
     private val viewModelShared : MainViewModel by activityViewModels()
+    private lateinit var animation: Animation
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,8 +38,9 @@ class PostList : Fragment() {
         val adapter = PostAdapter(viewModelShared, PostListener {
             viewModelShared.selectPost(it)
         })
+        animation = AnimationUtils.loadAnimation(context, R.anim.dismiss_all_recycler)
         binding.recyclerViewPosts.adapter = adapter
-        binding.recyclerViewPosts.itemAnimator = ItemAnimatorPost()
+        binding.recyclerViewPosts.itemAnimator = SlideInLeftAnimator()
 
         binding.viewModel = viewModelShared
 
@@ -51,11 +56,35 @@ class PostList : Fragment() {
             binding.swipeRefresh.isRefreshing = it
         })
         binding.swipeRefresh.setOnRefreshListener { viewModelShared.refresh() }
+
         viewModelShared.posts.observe(viewLifecycleOwner, Observer {
             it?.let {
-                adapter.submitList(it)
+                if(it.isNotEmpty()){
+                    if(binding.recyclerViewPosts.alpha == 0f) {
+                        binding.recyclerViewPosts.alpha = 1f
+                    }
+                    adapter.submitList(it)
+                } else {
+                    animation.setAnimationListener(object : Animation.AnimationListener {
+                        override fun onAnimationStart(p0: Animation?) {
+                        }
+
+                        override fun onAnimationEnd(p0: Animation?) {
+                            binding.recyclerViewPosts.alpha = 0f
+                            adapter.submitList(it) //Clear list for adapter
+                        }
+
+                        override fun onAnimationRepeat(p0: Animation?) {
+                        }
+
+                    })
+                    binding.recyclerViewPosts.startAnimation(animation)
+
+                }
+
             }
         })
+
         return binding.root
     }
 
